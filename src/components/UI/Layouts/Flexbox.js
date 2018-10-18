363bd233c8510926c172a5653dc74571c615e9eb
Prabeen04 <prabeen.strange@gmail.com>
2018-10-18 23:51:49 +0530

    flex-container styles added

--- src/components/UI/Layouts/Flexbox.js
+++ src/components/UI/Layouts/Flexbox.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 const Flexbox = styled.div`
-
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
 `
 export default Flexbox;
