1d687e82ff94aec130e97780cfccb0ca177df299
Prabeen04 <prabeen.strange@gmail.com>
2018-10-19 18:28:12 +0530

    small stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { ApplicationWrapper, Flexbox } from "./components/UI/Layouts";
-
+import { SearchInput } from 'evergreen-ui'
 class App extends Component {
   render() {
     return (
       <ApplicationWrapper>
+        <SearchInput placeholder="Filter traits..." width="100%" />
         <Flexbox>
           <h3>Evergreen UI</h3>
           <h3>Latest Library</h3>
